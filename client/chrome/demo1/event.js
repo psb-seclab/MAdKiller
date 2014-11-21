1db0a39c50600242415ef7123092c4b93dcd1b15
Thomas Roseman <lindows.dev@gmail.com>
2014-11-21 11:26:01 -0500

    Websocket added and tab url grabbing added

--- client/chrome/demo1/event.js
+++ client/chrome/demo1/event.js
@@ -1,19 +1,48 @@
-// This function will eventually contain some logic
-// for receiving background-color values from the
-// current tab.
-function getBgColors (tab) {
-  	// But for now, let's just make sure what we have so
-  	// far is working as expected.
-  	//alert('The browser action was clicked! Yay!');
-  	var socket = io('http://localhost');
-  	socket.on('news', function (data) {
-    	console.log(data);
-    	socket.emit('my other event', { my: 'data' });
-  	});
+// Opens up a socket to a python server and sends information about page
+function sendWebData() {
+  	// Alert test for Extension test
+  	console.log('The browser action was clicked! Yay!');
 
 
+    var serverURL = "ws://104.131.55.173:8888/";
+    //Basic websocket connection to a test server.
+
+    var soccketConnection = new WebSocket(serverURL);
+
+    //Send current URL
+    //Sends extension ID
+    sendMessage(document.URL, soccketConnection);
+
+    //Sends url from current tab
+    chrome.tabs.getSelected(null,function(tab) {
+        var tablink = tab.url;
+        sendMessage(tablink, soccketConnection);
+        console.log(tablink);
+    });
 }
 
+function sendMessage(msg, socket) {
+    waitForSocketConnection(socket, function() {
+        socket.send(msg);
+    });
+}
+
+//The browser needs a few seconds to establish a connection. Wait to send a message
+function waitForSocketConnection(socket, callback){
+    setTimeout(
+        function(){
+            if (socket.readyState === 1) {
+                if(callback !== undefined){
+                    callback();
+                }
+            } else {
+                waitForSocketConnection(socket,callback);
+            }
+        }, 5);
+}
+
+
 // When the browser action is clicked, call the
-// getBgColors function.
-chrome.browserAction.onClicked.addListener(getBgColors);
+// sendWebData function.
+chrome.browserAction.onClicked.addListener(sendWebData);
+sendWebData();
